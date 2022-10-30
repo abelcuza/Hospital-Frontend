7f314c2be8219a9e9942c7955bf6d7caea0e823b
Abel Cuza <abel.cuza07@gmail.com>
2022-10-30 18:51:21 -0400

    Add model name in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import TableView from "../pages/table-view";
-import MedicoModel, {medicoFields} from "../models/medico";
+import MedicoModel, {medicoFields, medicoModelName} from "../models/medico";
 import PacienteModel from "../models/paciente";
 import MedicamentoModel from "../models/medicamento";
 import ConsultaModel from "../models/consulta";
@@ -8,7 +8,7 @@ import medicoApi from "../api/medico";
 
 export const authRoutes = [
     {path: '/', page: <Home/>},
-    {path: '/medico', page: <TableView model={MedicoModel} fields={medicoFields} api={medicoApi}/>},
+    {path: '/medico', page: <TableView model={MedicoModel} fields={medicoFields} api={medicoApi} model_name={medicoModelName}/>},
     {path: '/paciente', page: <TableView model={PacienteModel}/>},
     {path: '/consulta', page: <TableView model={ConsultaModel}/>},
     {path: '/medicamento', page: <TableView model={MedicamentoModel}/>}
