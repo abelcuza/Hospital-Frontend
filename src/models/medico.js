7611d0b900718e22e8d4d8de7e4798e6e520de1a
Abel Cuza <abel.cuza07@gmail.com>
2022-10-29 20:18:30 -0400

    Add constructor in medico model

--- src/models/medico.js
+++ src/models/medico.js
@@ -1,7 +1,20 @@
-const MedicoModel = {
-    nombre: "",
-    apellidos: "",
-    get_fields: () => ["nombre", "apellidos"]
+export const medicoFields = ['nombre', 'apellidos', 'ci', 'email', 'direccion', 'categoria', 'especialidad', 'telefono']
+
+class MedicoModel {
+    constructor(nombre, apellidos, ci, email = "", direccion = "", categoria = "", especialidad = "", telefono = "", ...args) {
+        this.nombre = nombre
+        this.apellidos = apellidos
+        this.ci = ci
+        this.email = email
+        this.direccion = direccion
+        this.categoria = categoria
+        this.especialidad = especialidad
+        this.telefono = telefono
+    }
+    values = () => ([this.nombre, this.apellidos, this.ci, this.email, this.direccion, this.categoria, this.especialidad, this.telefono])
+    keys = () => (medicoFields)
 }
 
 export default MedicoModel;
+
+
