f92fd1660ceb98871e527aded3ca46de9edce9d5
Abel Cuza <abel.cuza07@gmail.com>
2022-10-29 20:21:43 -0400

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import TableView from "../pages/table-view";
-import MedicoModel from "../models/medico";
+import MedicoModel, {medicoFields} from "../models/medico";
 import PacienteModel from "../models/paciente";
 import MedicamentoModel from "../models/medicamento";
 import ConsultaModel from "../models/consulta";
 import Home from "../pages/home";
+import medicoApi from "../api/medico";
 
 export const authRoutes = [
     {path: '/', page: <Home/>},
-    {path: '/medico', page: <TableView model={MedicoModel}/>},
+    {path: '/medico', page: <TableView model={MedicoModel} fields={medicoFields} api={medicoApi}/>},
     {path: '/paciente', page: <TableView model={PacienteModel}/>},
     {path: '/consulta', page: <TableView model={ConsultaModel}/>},
     {path: '/medicamento', page: <TableView model={MedicamentoModel}/>}
