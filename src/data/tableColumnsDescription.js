c43eebce81fe2743252be2d2c50db99aa52c6365
Abel Cuza <abel.cuza07@gmail.com>
2022-10-30 18:52:03 -0400

    Delete unused table columns

--- src/data/tableColumnsDescription.js
+++ src/data/tableColumnsDescription.js
@@ -39,27 +39,6 @@ const tableColumnsDescription = {
         label: 'Categoría',
         minWidth: 170
     },
-    population: {
-        id: 'population',
-        label: 'Population',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    size: {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    density: {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
-    },
 };
 
 export default tableColumnsDescription
