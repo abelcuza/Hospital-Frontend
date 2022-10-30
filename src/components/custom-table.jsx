9d44ad2d84e6a8948c8b047db1bc1ceb1e426e75
Abel Cuza <abel.cuza07@gmail.com>
2022-10-29 20:19:33 -0400

    Change the design and add functions to fetch data

--- src/components/custom-table.jsx
+++ src/components/custom-table.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,44 +9,36 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import tableColumnsDescription from "../data/tableColumnsDescription";
 
-function createData(name, code, population, size) {
-    const density = population / size;
-    return {name, code, population, size, density};
+function createData(row, model) {
+    const item = new model()
+    Object.assign(item, row)
+    return item.values();
 }
 
-const rows = [
-    // createData('India', 'IN', 1324171354, 3287263),
-    // createData('China', 'CN', 1403500365, 9596961),
-    // createData('Italy', 'IT', 60483973, 301340),
-    // createData('United States', 'US', 327167434, 9833520),
-    // createData('Canada', 'CA', 37602103, 9984670),
-    // createData('Australia', 'AU', 25475400, 7692024),
-    // createData('Germany', 'DE', 83019200, 357578),
-    // createData('Ireland', 'IE', 4857000, 70273),
-    // createData('Mexico', 'MX', 126577691, 1972550),
-    // createData('Japan', 'JP', 126317000, 377973),
-    // createData('France', 'FR', 67022000, 640679),
-    // createData('United Kingdom', 'GB', 67545757, 242495),
-    // createData('Russia', 'RU', 146793744, 17098246),
-    // createData('Nigeria', 'NG', 200962417, 923768),
-    // createData('Brazil', 'BR', 210147125, 8515767),
-];
+const CustomTable = ({fields, api, model}) => {
+    const [data, setData] = useState(0)
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
 
-const CustomTable = ({fields, values}) => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    async function fetchData() {
+        return await api.list()
+    }
+
+    useEffect(() => {
+        fetchData().then((data) => setData(data.data))
+    }, [])
+
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
 
     return (
-        <Paper sx={{width: '100%', overflow: 'hidden'}}>
+        <Paper sx={{width: '100%'}}>
             <TableContainer sx={{maxHeight: 440}}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
@@ -63,31 +55,23 @@ const CustomTable = ({fields, values}) => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {/*{rows*/}
-                        {/*    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)*/}
-                        {/*    .map((row) => {*/}
-                        {/*        return (*/}
-                        {/*            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>*/}
-                        {/*                {columns.map((column) => {*/}
-                        {/*                    const value = row[column.id];*/}
-                        {/*                    return (*/}
-                        {/*                        <TableCell key={column.id} align={column.align}>*/}
-                        {/*                            {column.format && typeof value === 'number'*/}
-                        {/*                                ? column.format(value)*/}
-                        {/*                                : value}*/}
-                        {/*                        </TableCell>*/}
-                        {/*                    );*/}
-                        {/*                })}*/}
-                        {/*            </TableRow>*/}
-                        {/*        );*/}
-                        {/*    })}*/}
+                        {data ?
+                            data.map((row) => (
+                                <TableRow>
+                                    {createData(row, model).map((val) => (
+                                        <TableCell key={val}>{val}</TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                            : null
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
             <TablePagination
                 rowsPerPageOptions={[10, 25, 100]}
                 component="div"
-                count={rows.length}
+                count={data.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={handleChangePage}
