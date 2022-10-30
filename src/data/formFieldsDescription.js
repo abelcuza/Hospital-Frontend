c40f66e7c856a2f91f16f1a3c40369a86aac96e7
Abel Cuza <abel.cuza07@gmail.com>
2022-10-30 18:52:41 -0400

    Add form field descriptions

---
+++ src/data/formFieldsDescription.js
@@ -0,0 +1,44 @@
+const formFieldsDescription = {
+    nombre: {
+        id: 'nombre',
+        label: 'Nombre',
+        required: true
+    },
+    apellidos: {
+        id: 'apellidos',
+        label: 'Apellidos',
+        required: true
+    },
+    ci: {
+        id: 'ci',
+        label: 'CI',
+        required: true
+    },
+    direccion: {
+        id: 'direccion',
+        label: 'Dirección',
+        required: true
+    },
+    email: {
+        id: 'email',
+        label: 'Email',
+        required: true
+    },
+    especialidad: {
+        id: 'especialidad',
+        label: 'Especialidad',
+        required: false
+    },
+    telefono: {
+        id: 'telefono',
+        label: 'Teléfono',
+        required: true
+    },
+    categoria: {
+        id: 'categoria',
+        label: 'Categoría',
+        required: false
+    },
+}
+
+export default formFieldsDescription
