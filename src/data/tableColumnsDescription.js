28a13b0c364e881a99a820249b76924a383a1039
Abel Cuza <abel.cuza07@gmail.com>
2022-10-29 20:18:49 -0400

    Add medico fields in table column description

--- src/data/tableColumnsDescription.js
+++ src/data/tableColumnsDescription.js
@@ -14,6 +14,31 @@ const tableColumnsDescription = {
         label: 'CI',
         minWidth: 170
     },
+    direccion: {
+        id: 'direccion',
+        label: 'Dirección',
+        minWidth: 170
+    },
+    email: {
+        id: 'email',
+        label: 'Email',
+        minWidth: 170
+    },
+    especialidad: {
+        id: 'especialidad',
+        label: 'Especialidad',
+        minWidth: 170
+    },
+    telefono: {
+        id: 'telefono',
+        label: 'Teléfono',
+        minWidth: 170
+    },
+    categoria: {
+        id: 'categoria',
+        label: 'Categoría',
+        minWidth: 170
+    },
     population: {
         id: 'population',
         label: 'Population',
