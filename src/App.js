d79661ad4998413d5fbdfc51f81fac1d52067276
Abel Cuza <abel.cuza07@gmail.com>
2022-11-02 17:02:22 -0400

    Add useRoutes hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import './App.css';
-import Dashboard from "./pages/dashboard";
+import {useRoutes} from "react-router";
+import {authRoutes} from "./routes";
 
 function App() {
+    const routes = useRoutes([
+        ...authRoutes
+    ])
     return (
         <div className="App">
-            <Dashboard/>
+            {routes}
         </div>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,38 @@
-import TableView from "../pages/table-view";
+import ContentView from "../pages/content-view";
 import MedicoModel, {medicoFields, medicoModelName} from "../models/medico";
 import PacienteModel from "../models/paciente";
 import MedicamentoModel from "../models/medicamento";
 import ConsultaModel from "../models/consulta";
-import Home from "../pages/home";
 import medicoApi from "../api/medico";
+import Dashboard from "../pages/dashboard";
+import CreateView from "../pages/create-view";
 
 export const authRoutes = [
-    {path: '/', page: <Home/>},
-    {path: '/medico', page: <TableView model={MedicoModel} fields={medicoFields} api={medicoApi} model_name={medicoModelName}/>},
-    {path: '/paciente', page: <TableView model={PacienteModel}/>},
-    {path: '/consulta', page: <TableView model={ConsultaModel}/>},
-    {path: '/medicamento', page: <TableView model={MedicamentoModel}/>}
+    {
+        path: '/',
+        element: <Dashboard/>,
+        children: [
+            {
+                path: '/medico',
+                element: <ContentView model={MedicoModel} fields={medicoFields} api={medicoApi}
+                                      model_name={medicoModelName}/>,
+                children: [
+                    {
+                        path: '/medico/add',
+                        element: <CreateView model={MedicoModel} fields={medicoFields} api={medicoApi}
+                                             model_name={medicoModelName}/>
+                    },
+                    {
+                        path: '/medico/:id',
+                        element: <CreateView model={MedicoModel} fields={medicoFields} api={medicoApi}
+                                             model_name={medicoModelName}/>
+                    },
+                ]
+            },
+            {path: '/paciente', element: <ContentView model={PacienteModel}/>},
+            {path: '/consulta', element: <ContentView model={ConsultaModel}/>},
+            {path: '/medicamento', element: <ContentView model={MedicamentoModel}/>}
+        ]
+    },
+
 ]
