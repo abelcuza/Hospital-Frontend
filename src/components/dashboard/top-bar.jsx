0754c4ba952294d1df1de0ad182a4645a18f6847
Abel Cuza <abel.cuza07@gmail.com>
2022-11-10 19:18:04 -0500

    Fix top bar right menu

--- src/components/dashboard/top-bar.jsx
+++ src/components/dashboard/top-bar.jsx
@@ -5,8 +5,26 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
 import {styled} from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
+import LocalHospitalSharpIcon from '@mui/icons-material/LocalHospitalSharp';
+import {Menu, MenuItem, Tooltip} from "@mui/material";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import {Link} from "react-router-dom";
+import settings from "../../data/tob-bar-options";
 
 const drawerWidth = 240;
+
+const logoStyle = {
+    display: "flex",
+    alignItems:"center"
+}
+
+const linkStyle = () => ({
+    color: 'rgba(0, 0, 0, 0.87)',
+    textDecoration: 'NONE',
+    display: "flex",
+    alignItems:"center"
+})
 
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
@@ -26,28 +44,96 @@ const AppBar = styled(MuiAppBar, {
     }),
 }));
 
-const TopBar = ({open, action}) => (
-    <div className='top-bar'>
-        <AppBar position="fixed" open={open}>
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={action}
-                    edge="start"
-                    sx={{
-                        marginRight: 5,
-                        ...(open && {display: 'none'}),
-                    }}
-                >
-                    <MenuIcon/>
-                </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                    El consultorio sabroso
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </div>
-)
+
+const TopBar = ({open, action, user}) => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+    return (
+        <div className='top-bar'>
+            <AppBar position="fixed" open={open}>
+                <Toolbar sx={{display: {xs: 'flex', md: 'flex'}, justifyContent: {xs: 'center', md: 'space-between'}}}>
+                    <div className="logo" style={logoStyle}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={action}
+                            edge="start"
+                            sx={{
+                                marginRight: 5,
+                                ...(open && {display: 'none'}),
+                            }}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <LocalHospitalSharpIcon sx={{display: {xs: 'flex', md: 'flex'}, mr: 1}}/>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: {xs: 'flex', md: 'flex'},
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            VPOLICLÍNICO
+                        </Typography>
+                    </div>
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                <Avatar alt={user.toUpperCase()} src="/static/images/avatar/2.jpg"/>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{mt: '45px'}}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map(({name, icon, path}) => (
+                                <MenuItem key={name}>
+                                    <Link style={linkStyle()} to={path}>
+                                        {icon}
+                                    <Typography textAlign="center">{name}</Typography>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
 
 export default TopBar;
