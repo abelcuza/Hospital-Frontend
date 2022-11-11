b679dc4eca168ff4796d5c6242474385d94f3dc6
Abel Cuza <abel.cuza07@gmail.com>
2022-11-10 19:14:57 -0500

    Update medico api

--- src/api/medico.js
+++ src/api/medico.js
@@ -3,8 +3,8 @@ import getApi from "./base/get-api";
 const {REACT_APP_BACKEND_API} = process.env;
 const endpoint = `${REACT_APP_BACKEND_API}/medicos/`;
 
-const medicoApi = {
-    ...getApi(endpoint)
-}
+const medicoApi = (id="")=> ({
+    ...getApi(endpoint, id)
+})
 
 export default medicoApi
