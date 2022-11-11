9b84f6b444916c265cb7f1d19e5c23565f0ea601
Abel Cuza <abel.cuza07@gmail.com>
2022-11-10 19:20:39 -0500

    Add auth in axios

--- src/api/base/index.js
+++ src/api/base/index.js
@@ -1,11 +1,20 @@
-import axios from 'axios'
+import getAxiosInstance from "./get-instance";
+
+
+const contentType = 'application/json'
+const axios = getAxiosInstance()
 
 export const get = async (url, params) => (
     axios.get(url, {params})
 )
 
-export const post = async (url, data) => (
-    axios.post(url, data)
+export const post = async (url, data, headers) => (
+    axios.post(url, data, {
+        headers: {
+            'Content-Type': contentType,
+            ...headers
+        }
+    })
 )
 
 export const put = async (url, data) => (
