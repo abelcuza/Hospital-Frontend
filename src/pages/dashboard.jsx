ff12f73080cb3f5091a47c96d44df733d77b42b5
Abel Cuza <abel.cuza07@gmail.com>
2022-11-10 19:33:52 -0500

    Add conditional rendering if user is logged

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import TopBar from "../components/dashboard/top-bar";
 import SideBar from "../components/dashboard/side-bar";
 import DrawerHeader from "../components/dashboard";
-import {Outlet} from "react-router";
+import {Outlet, Navigate} from "react-router";
+import Copyright from "../components/copyright";
+import {useSelector} from "react-redux";
 
 
 const Dashboard = () => {
+    const auth = useSelector((state) => state.user.isAuthenticated)
+    const [user, setUser] = useState("")
     const theme = useTheme();
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = useState(true);
 
+    useEffect(() => {
+        setUser(localStorage.getItem("username"))
+    }, [])
     const handleDrawerOpen = () => {
         setOpen(true);
     };
@@ -20,16 +27,23 @@ const Dashboard = () => {
         setOpen(false);
     };
 
+    if(!auth){
+        return <Navigate to="/login"/>
+    }
+
     return (
-        <Box sx={{display: 'flex'}}>
-            <CssBaseline/>
-            <TopBar open={open} action={handleDrawerOpen}/>
-            <SideBar open={open} action={handleDrawerClose} theme={theme}/>
-            <Box component="main" sx={{flexGrow: 1, p: 3, paddingTop: 3}}>
-                <DrawerHeader/>
-                <Outlet/>
+        <div className="dashboard">
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline/>
+                <TopBar open={open} user={user} action={handleDrawerOpen}/>
+                <SideBar open={open} action={handleDrawerClose} theme={theme}/>
+                <Box component="main" sx={{flexGrow: 1, p: 3, paddingTop: 3}}>
+                    <DrawerHeader/>
+                    <Outlet/>
+                </Box>
             </Box>
-        </Box>
+            <Copyright sx={{mt: 8, mb: 4}}/>
+        </div>
     );
 }
 
