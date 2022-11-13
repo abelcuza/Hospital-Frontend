f3b860b08beb074fd1a6a5b93ccd08b0a0350aab
Abel Cuza <abel.cuza07@gmail.com>
2022-11-13 14:44:25 -0500

    Add a filter bar

---
+++ src/components/filters.jsx
@@ -0,0 +1,26 @@
+import Box from "@mui/material/Box";
+import {Autocomplete, TextField} from "@mui/material";
+import formFieldsDescription from "../data/formFieldsDescription";
+
+
+const SelectFilter = ({filters, inputFilters, setInputFilters}) => (
+        <Box sx={{width: 300, maxWidth: '100%'}}>
+            <Autocomplete
+                value={inputFilters}
+                onChange={(e, value) => (
+                    setInputFilters(value)
+                )}
+                multiple
+                size="small"
+                id="checkboxes-tags-demo"
+                options={filters}
+                getOptionLabel={(option) => formFieldsDescription[option].label}
+                renderInput={(params) => (
+                    <TextField  {...params} label="Filtros" placeholder=""/>
+                )}
+            />
+        </Box>
+    )
+
+
+export default SelectFilter
