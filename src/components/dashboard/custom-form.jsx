f6de610c94b6127c13541809218f33d5f4dc2da6
Abel Cuza <abel.cuza07@gmail.com>
2022-11-15 01:05:10 -0500

    Change text field for custom field

--- src/components/dashboard/custom-form.jsx
+++ src/components/dashboard/custom-form.jsx
@@ -1,40 +1,23 @@
 import React, {useEffect} from "react"
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import formFieldsDescription from "../../data/formFieldsDescription";
-
-const input_style = {
-    width: "100%"
-}
+import CustomField from "../custom-field";
 
 const CustomForm = ({fields, setParams, values}) => {
     useEffect(() => {
         setParams(values)
     }, [])
-    const handleChange = (event) => {
-        const {name, value} = event.target
-        setParams(data => {
-            return {...data, [name]: value}
-        })
-    }
+
     return (
         <div className="custom-form">
             <Grid container spacing={2}>
                 {fields.map((field) => (
                     <Grid key={field} item xs={12}>
                         {values ?
-                            <TextField defaultValue={values[field]} style={input_style} size="small"
-                                       onChange={handleChange}
-                                       required={formFieldsDescription[field].required}
-                                       id={formFieldsDescription[field].id}
-                                       name={formFieldsDescription[field].id}
-                                       label={formFieldsDescription[field].label}/>
+                            <CustomField defaultValue={values[field]} fullWidth={true}
+                                         setParams={setParams} {...formFieldsDescription[field]}/>
                             :
-                            <TextField style={input_style} size="small" onChange={handleChange}
-                                       required={formFieldsDescription[field].required}
-                                       id={formFieldsDescription[field].id}
-                                       name={formFieldsDescription[field].id}
-                                       label={formFieldsDescription[field].label}/>
+                            <CustomField fullWidth={true} setParams={setParams} {...formFieldsDescription[field]}/>
                         }
                     </Grid>
                 ))}
