3eae55a573c8255697a24bc6c95f7010b67c14b8
Abel Cuza <abel.cuza07@gmail.com>
2022-11-15 01:11:24 -0500

    Remove handle change and unused imports

--- src/components/filters-form.jsx
+++ src/components/filters-form.jsx
@@ -1,20 +1,15 @@
 import React from "react"
-import {Stack, TextField} from "@mui/material";
+import {Stack} from "@mui/material";
 import formFieldsDescription from "../data/formFieldsDescription";
 import CustomField from "./custom-field";
 
 const FiltersForm = ({inputFilters, setFilterParams}) => {
-    const handleChange = (event) => {
-        const {name, value} = event.target
-        setFilterParams(data => {
-            return {...data, [name]: value}
-        })
-    }
     return (
         <div className="filters-form">
             <Stack direction="row" sx={{marginBottom: "50px", marginTop: "20px"}} spacing={2}>
                 {inputFilters.map(filter => (
-                    <CustomField {...formFieldsDescription[filter]} setParams={setFilterParams} fullWidth={false} width={"200px"}/>
+                    <CustomField {...formFieldsDescription[filter]} setParams={setFilterParams} fullWidth={false}
+                                 width={"200px"}/>
                 ))}
             </Stack>
         </div>
