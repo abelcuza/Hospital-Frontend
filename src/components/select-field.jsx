7e07c005951996046ee364f74951ff8bcdb8f9ea
Abel Cuza <abel.cuza07@gmail.com>
2022-11-15 01:03:13 -0500

    Add a custom select field

---
+++ src/components/select-field.jsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react"
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+
+const SelectField = ({id, label, values, required, setParams, fullWidth, width, defaultValue}) => {
+    const [selectValue, setSelectValue] = useState("")
+
+    useEffect(()=>{
+        if (defaultValue){
+            setSelectValue(defaultValue)
+        }
+    }, [])
+
+    const handleChange = (event) => {
+        const {value} = event.target
+        setSelectValue(value)
+        setParams(data => {
+            return {...data, [id]: value}
+        })
+    }
+    return (
+        defaultValue ?
+            <FormControl required={required} fullWidth={fullWidth} sx={{width: width}} size="small">
+                <InputLabel id={`${id}-label`}>{label}</InputLabel>
+                <Select
+                    id={`${id}_select`}
+                    labelId={`${id}_label`}
+                    value={selectValue}
+                    label={label}
+                    onChange={handleChange}
+                >
+                    {values.map(({id, name}) => (
+                        <MenuItem key={id} value={id}>{name}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            :
+            <FormControl required={required} fullWidth={fullWidth} sx={{width: width}} size="small">
+                <InputLabel id={`${id}-label`}>{label}</InputLabel>
+                <Select
+                    id={`${id}-select`}
+                    labelId={`${id}-label`}
+                    value={selectValue}
+                    label={label}
+                    onChange={handleChange}
+                >
+                    {values.map(({id, name}) => (
+                        <MenuItem key={id} value={id}>{name}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+    )
+}
+
+export default SelectField
