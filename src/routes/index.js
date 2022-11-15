ff98339f139cfc3ba6c4f12b6dde917e24059d97
Abel Cuza <abel.cuza07@gmail.com>
2022-11-15 01:02:51 -0500

    Add add, update and delete for all routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
+import models from "../models";
 import ContentView from "../pages/content-view";
-import MedicoModel, {medicoFields, medicoModelName} from "../models/medico";
-import PacienteModel from "../models/paciente";
-import MedicamentoModel from "../models/medicamento";
-import ConsultaModel from "../models/consulta";
 import medicoApi from "../api/medico";
 import Dashboard from "../pages/dashboard";
 import CreateView from "../pages/create-view";
 import DeleteMessage from "../pages/delete-message";
 import Login from "../pages/login";
 import Logout from "../pages/logout";
+import pacienteApi from "../api/paciente";
+import medicamentoApi from "../api/medicamento";
+import consultaApi from "../api/consulta";
+
+const {medico, paciente, consulta, medicamento} = models
 
 export const authRoutes = [
     {
@@ -17,18 +19,18 @@ export const authRoutes = [
         children: [
             {
                 path: '/medico',
-                element: <ContentView model={MedicoModel} fields={medicoFields} api={medicoApi}
-                                      model_name={medicoModelName}/>,
+                element: <ContentView model={medico.model} fields={Object.keys(medico.model)} api={medicoApi}
+                                      model_name={medico.modelName}/>,
                 children: [
                     {
                         path: '/medico/add',
-                        element: <CreateView model={MedicoModel} fields={medicoFields} api={medicoApi}
-                                             model_name={medicoModelName}/>
+                        element: <CreateView model={medico.model} fields={Object.keys(medico.model)} api={medicoApi}
+                                             model_name={medico.modelName}/>
                     },
                     {
                         path: '/medico/:id',
-                        element: <CreateView model={MedicoModel} fields={medicoFields} api={medicoApi}
-                                             model_name={medicoModelName}/>
+                        element: <CreateView model={medico.model} fields={Object.keys(medico.model)} api={medicoApi}
+                                             model_name={medico.modelName}/>
                     },
                     {
                         path: '/medico/:id/delete',
@@ -36,9 +38,69 @@ export const authRoutes = [
                     },
                 ]
             },
-            {path: '/paciente', element: <ContentView model={PacienteModel}/>},
-            {path: '/consulta', element: <ContentView model={ConsultaModel}/>},
-            {path: '/medicamento', element: <ContentView model={MedicamentoModel}/>},
+            {
+                path: '/paciente',
+                element: <ContentView model={paciente.model} fields={Object.keys(paciente.model)} api={pacienteApi}
+                                      model_name={paciente.modelName}/>,
+                children: [
+                    {
+                        path: '/paciente/add',
+                        element: <CreateView model={paciente.model} fields={Object.keys(paciente.model)} api={pacienteApi}
+                                             model_name={paciente.modelName}/>
+                    },
+                    {
+                        path: '/paciente/:id',
+                        element: <CreateView model={paciente.model} fields={Object.keys(paciente.model)} api={pacienteApi}
+                                             model_name={paciente.modelName}/>
+                    },
+                    {
+                        path: '/paciente/:id/delete',
+                        element: <DeleteMessage api={pacienteApi}/>
+                    },
+                ]
+            },
+            {
+                path: '/consulta',
+                element: <ContentView model={consulta.model} fields={Object.keys(consulta.model)}
+                                                         api={consultaApi} model_name={consulta.modelName}/>,
+                children: [
+                    {
+                        path: '/consulta/add',
+                        element: <CreateView model={consulta.model} fields={Object.keys(consulta.model)} api={consultaApi}
+                                             model_name={consulta.modelName}/>
+                    },
+                    {
+                        path: '/consulta/:id',
+                        element: <CreateView model={consulta.model} fields={Object.keys(consulta.model)} api={consultaApi}
+                                             model_name={consulta.modelName}/>
+                    },
+                    {
+                        path: '/consulta/:id/delete',
+                        element: <DeleteMessage api={consultaApi}/>
+                    },
+                ]
+            },
+            {
+                path: '/medicamento',
+                element: <ContentView model={medicamento.model} fields={Object.keys(medicamento.model)}
+                                      api={medicamentoApi} model_name={medicamento.modelName}/>,
+                children: [
+                    {
+                        path: '/medicamento/add',
+                        element: <CreateView model={medicamento.model} fields={Object.keys(medicamento.model)} api={medicamentoApi}
+                                             model_name={medicamento.modelName}/>
+                    },
+                    {
+                        path: '/medicamento/:id',
+                        element: <CreateView model={medicamento.model} fields={Object.keys(medicamento.model)} api={medicamentoApi}
+                                             model_name={medicamento.modelName}/>
+                    },
+                    {
+                        path: '/medicamento/:id/delete',
+                        element: <DeleteMessage api={medicamentoApi}/>
+                    },
+                ]
+            },
 
         ]
     }
