ae6e4646d4c2d18ccd6110deeea4e639fbc6b505
Abel Cuza <abel.cuza07@gmail.com>
2022-11-15 01:02:30 -0500

    Add type and select values for form and table collumns

--- src/data/formFieldsDescription.js
+++ src/data/formFieldsDescription.js
@@ -33,7 +33,29 @@ const formFieldsDescription = {
         id: 'especialidad',
         label: 'Especialidad',
         required: true,
-        type: 'text'
+        type: 'select',
+        values: [
+            {
+                id: "mgi",
+                name:"Médico General Integral"
+            },
+            {
+                id: "ginecologo",
+                name:"Ginecologo"
+            },
+            {
+                id: "geriatra",
+                name:"Geriatra"
+            },
+            {
+                id: "pediatra",
+                name:"Pediatra"
+            },
+            {
+                id: "imagenologo",
+                name:"Imagenólogo"
+            },
+        ]
     },
     telefono: {
         id: 'telefono',
@@ -41,9 +63,117 @@ const formFieldsDescription = {
         required: true,
         type: 'text'
     },
-    categoria: {
-        id: 'categoria',
-        label: 'Categoría',
+    grado_academico: {
+        id: 'grado_academico',
+        label: 'Grado Académico',
+        required: true,
+        type: 'select',
+        values: [
+            {
+                id: "universitario",
+                name:"Universitario"
+            },
+            {
+                id: "master",
+                name:"Master en Ciencias"
+            },
+            {
+                id: "doctor",
+                name:"Doctor en Ciencias"
+            }
+        ]
+    },
+    municipio: {
+        id: 'municipio',
+        label: 'Municipio',
+        required: true,
+        type: 'text'
+    },
+    provincia: {
+        id: 'provincia',
+        label: 'Provincia',
+        required: true,
+        type: 'text'
+    },
+    tipo: {
+        id: 'tipo',
+        label: 'Tipo',
+        required: true,
+        type: 'select',
+        values: [
+            {
+                id: "analgesico",
+                name:"Analgésico"
+            },
+            {
+                id: "antiinflamatorio",
+                name:"Antiinflamatorio"
+            },
+            {
+                id: "antiacido",
+                name:"Antiácido"
+            },
+            {
+                id: "antipiretico",
+                name:"Antipirético"
+            },
+            {
+                id: "antiinfeccioso",
+                name:"Antiinfeccioso"
+            },
+            {
+                id: "antitusivo_mucolitico",
+                name:"Antitusivo y mucolítico"
+            },
+            {
+                id: "antialergico",
+                name:"Antialérgico"
+            },
+            {
+                id: "antidiarreico_laxante",
+                name:"Antidiarreico y Laxante"
+            },
+        ]
+    },
+    nombre_generico: {
+        id: 'nombre_generico',
+        label: 'Nombre Genérico',
+        required: true,
+        type: 'text'
+    },
+    nombre_comercial: {
+        id: 'nombre_comercial',
+        label: 'Nombre Comercial',
+        required: true,
+        type: 'text'
+    },
+    medico: {
+        id: 'medico',
+        label: 'Médico',
+        required: true,
+        type: 'text'
+    },
+    paciente: {
+        id: 'paciente',
+        label: 'Paciente',
+        required: true,
+        type: 'model'
+    },
+    fecha: {
+        id: 'fecha',
+        label: 'Fecha',
+        required: true,
+        type: 'date'
+    },
+    medicamentos: {
+        id: 'medicamentos',
+        label: 'Medicamentos',
+        required: true,
+        type: 'model'
+    },
+    diagnostico: {
+        id: 'diagnostico',
+        label: 'Diagnóstico',
         required: true,
         type: 'text'
     },
--- src/data/tableColumnsDescription.js
+++ src/data/tableColumnsDescription.js
@@ -1,43 +1,140 @@
 const tableColumnsDescription = {
+    id: {
+        id: 'id',
+        label: 'ID',
+        type: 'text'
+    },
     nombre: {
         id: 'nombre',
         label: 'Nombre',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
+
     },
     apellidos: {
         id: 'apellidos',
         label: 'Apellidos',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
+
     },
     ci: {
         id: 'ci',
         label: 'CI',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
     },
     direccion: {
         id: 'direccion',
         label: 'Dirección',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
     },
     email: {
         id: 'email',
         label: 'Email',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
     },
     especialidad: {
         id: 'especialidad',
         label: 'Especialidad',
-        minWidth: 170
+        minWidth: 170,
+        type: 'select',
+        values: {
+            mgi: "Médico General Integral",
+            ginecologo: "Ginecologo",
+            geriatra: "Geriatra",
+            pediatra: "Pediatra",
+            imagenologo: "Imagenólogo",
+        }
     },
     telefono: {
         id: 'telefono',
         label: 'Teléfono',
-        minWidth: 170
+        minWidth: 170,
+        type: 'text'
     },
-    categoria: {
-        id: 'categoria',
-        label: 'Categoría',
-        minWidth: 170
+    grado_academico: {
+        id: 'grado_academico',
+        label: 'Grado Académico',
+        minWidth: 170,
+        type: 'select',
+        values: {
+            universitario: "Universitario",
+            master: "Master en Ciencias",
+            doctor: "Doctor en Ciencias",
+        }
+    },
+    municipio: {
+        id: 'municipio',
+        label: 'Municipio',
+        minWidth: 170,
+        type: 'text'
+    },
+    provincia: {
+        id: 'provincia',
+        label: 'Provincia',
+        minWidth: 170,
+        type: 'text'
+    },
+    tipo: {
+        id: 'tipo',
+        label: 'Tipo',
+        minWidth: 170,
+        type: 'select',
+        values: {
+            analgesico: "Analgésico",
+            antiinflamatorio: "Antiinflamatorio",
+            antiacido: "Antiácido",
+            antipiretico: "Antipirético",
+            antiinfeccioso: "Antiinfeccioso",
+            antantitusivo_mucoliticoiacido: "Antitusivo y mucolítico",
+            antialergico: "Antialérgico",
+            antidiarreico_laxante: "Antidiarreico y Laxante",
+        }
+    },
+    nombre_generico: {
+        id: 'nombre_generico',
+        label: 'Nombre Genérico',
+        minWidth: 170,
+        type: 'text'
+    },
+    nombre_comercial: {
+        id: 'nombre_comercial',
+        label: 'Nombre Comercial',
+        minWidth: 170,
+        type: 'text'
+    },
+    medico: {
+        id: 'medico',
+        label: 'Médico',
+        minWidth: 170,
+        type: 'text'
+    },
+    paciente: {
+        id: 'paciente',
+        label: 'Paciente',
+        minWidth: 170,
+        type: 'text'
+    },
+    fecha: {
+        id: 'fecha',
+        label: 'Fecha',
+        minWidth: 170,
+        type: 'datetime'
+    },
+    medicamentos: {
+        id: 'medicamentos',
+        label: 'Medicamentos',
+        minWidth: 170,
+        type: 'list'
+    },
+    diagnostico: {
+        id: 'diagnostico',
+        label: 'Diagnóstico',
+        minWidth: 170,
+        type: 'pharagraph'
     },
 };
 
